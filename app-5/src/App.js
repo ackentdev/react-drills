7407049e5c6d4ca464f7403478919f7cf07c926a
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-06-25 20:34:03 -0700

    fixed image

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Image from "./Image";
 
@@ -7,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Image myImage={"https://via.placeholder.com/150"} />
+        <Image myImage={"https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg"} />
       </div>
     );
   }
